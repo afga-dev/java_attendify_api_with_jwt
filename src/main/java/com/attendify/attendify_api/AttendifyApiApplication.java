04e25c8e368eb9e2b1a55a4d36fd593d1f1371ae
Ana Guerra <afga.work.contact@gmail.com>
2025-11-07 09:24:11 -0600

    chore: initial commit

---
+++ src/main/java/com/attendify/attendify_api/AttendifyApiApplication.java
@@ -0,0 +1,13 @@
+package com.attendify.attendify_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AttendifyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AttendifyApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/attendify/attendify_api/AttendifyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.attendify.attendify_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AttendifyApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
